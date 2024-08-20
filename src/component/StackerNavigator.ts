c525e55073865971054aadd12157bfe5757596f9
DIEGO GABRIEL FERRARI <0220832221029@fatectq.local>
2024-08-20 12:16:41 -0300

    type

--- src/component/StackerNavigator.ts
+++ src/component/StackerNavigator.ts
@@ -1,11 +1,10 @@
 // StackNavigator.js
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from "../telas/Login";
 import Registrar from "../telas/Registrar";
 
 //  configurar  - <Stack.Screen name="Home" component={Home}/>
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function StackNavigator({user}) {
   return ( 
